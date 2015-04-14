e17fdfbbdb8017333b208078c722c5bb42b9dba4
Andrew Williams <andrewleighwilliams@gmail.com>
2015-04-14 10:04:55 -0400

    add in bio object to resumeBuilder

---
+++ js/resumeBuilder.js
@@ -0,0 +1,18 @@
+var bio {
+	"name": "Andrew Williams",
+	"role": "Front-end Web Developer",
+	"contacts": {
+		"mobile": "904-238-9414",
+		"email": "andrewleighwilliams@gmail.com"
+		"github": "ballsrichter"
+		"twitter": "@ballsrichter"
+		"location": "Jacksonville"
+	},
+	"welcomeMessage": "Welcoem to the website guy and other cool stuff.",
+	"skills": [
+		"HTML","CSS","Javascript","Bootstrap"
+	],
+	"biopic": "images/fry.jpg"
+}
+
+$(#main).append(bio.name,bio.role,bio.phone);
